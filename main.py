d98c8112f62f647588258617fbdd4876a92bda72
Matus Stas <matusstas.ms@gmail.com>
2020-01-04 22:12:24 +0100

    Added functionality (clean grid) + Refactoring

--- main.py
+++ main.py
@@ -1,94 +1,93 @@
 from PIL import Image, ImageDraw
-import PIL
+from getch import getch
 from tkinter import *
 import time
-from getch import getch
+import PIL
 
-# default color
 color = 'red'
+on = 0
 
-def set_red():
-	global color
-	color = 'red'
-
-def set_blue():
-	global color
-	color = 'blue'
-
-def set_white():
-	global color
-	color = 'white'
-
-def set_black():
-	global color
-	color = 'black'
-
-def get_size():
+def set_size():
 	width = 60
 	height = 25
 	pixel_size = 30
 	return width*pixel_size, height*pixel_size, pixel_size
 
-width, height, pixel_size = get_size()
 
-#this blok is used to drawing all objects in the grid like obstacles, start point, end point, erasing
+def create_grid(width, height, pixel_size):	#making the grid
+	for row in range(pixel_size, height, pixel_size):
+		screen.create_line(0, row, width, row, fill = "grey")
+	for column in range(pixel_size, width, pixel_size):
+		screen.create_line(column, 0, column, height, fill = "grey")
+
+
+def set_color(colour):
+	global color
+	color = colour
+
 
 def paint(event):
 	global color		
-	x1,y1 = (event.x)//pixel_size, (event.y)//pixel_size
-	platno.create_rectangle(x1*pixel_size+1,y1*pixel_size+1,x1*pixel_size+pixel_size-1,y1*pixel_size+pixel_size-1, fill = color, outline = color)
-	draw.rectangle([x1*pixel_size+1,y1*pixel_size+1,x1*pixel_size+pixel_size-1,y1*pixel_size+pixel_size-1], fill = color, outline = color)
+	x1, y1 = (event.x)//pixel_size, (event.y)//pixel_size
+	screen.create_rectangle(x1*pixel_size+1, y1*pixel_size+1, x1*pixel_size+pixel_size-1, y1*pixel_size+pixel_size-1, fill = color, outline = color)
+	draw.rectangle([x1*pixel_size+1, y1*pixel_size+1, x1*pixel_size+pixel_size-1, y1*pixel_size+pixel_size-1], fill = color, outline = color)
 
 
-def pixel_color(x,y):					#getting the color of current pixel in X Y axis
-	r,g,b = image.getpixel((x+1,y+1))
-	return r,g,b
+def pixel_color(x, y):
+	r, g, b = image.getpixel((x+1, y+1))
+	return r, g, b
 
-			
 
-def make():								#makes 2d array defined with 0s => if point was visited then changed to coordinates										
-	arr = []							#also function will find start point + end point
+def erase_all():
+	global on
+	if not(on):
+		for column in range(0, height, pixel_size):
+			for row in range(0, width, pixel_size):
+				screen.create_rectangle(row+1, column+1, row+pixel_size-1, column+pixel_size-1, fill = 'white', outline = 'white')
+				draw.rectangle([row+1, column+1, row+pixel_size-1, column+pixel_size-1], fill = 'white', outline = 'white')
+
+
+def make():																		
+	arr = []
 	count = 0
-	for riadok in range(0,width,pixel_size):
+	for row in range(0, width, pixel_size):
 		arr.append([])
-		for stlpec in range(0,height,pixel_size):
-			arr[riadok//pixel_size].append(0)
+		for column in range(0, height, pixel_size):
+			arr[row//pixel_size].append(0)
 
-			if(pixel_color(riadok,stlpec) == (255,0,0)):
-				start = [riadok,stlpec]
+			if(pixel_color(row, column) == (255, 0, 0)):
+				start = [row, column]
 				count += 1
-			if(pixel_color(riadok,stlpec) == (0,0,255)):
-				end = [riadok,stlpec]
+			if(pixel_color(row, column) == (0, 0, 255)):
+				end = [row, column]
 				count += 1
 
-	return arr,start,end
+	return arr, start, end
 
 
-
-def poinf_info(x,y):					#searching for all neighbours
-	info = []							#if pixel is black,grey == obstacle,visited pixel == no added to neighbour 
-	if(not(y-pixel_size < 0) and pixel_color(x,y-pixel_size) != (0,0,0) and pixel_color(x,y-pixel_size) != (128,128,128)):
-		info.append([x,y-pixel_size])
-	if(not(x+pixel_size+1 > width) and pixel_color(x+pixel_size,y) != (0,0,0) and pixel_color(x+pixel_size,y) != (128,128,128)):
-		info.append([x+pixel_size,y])
-	if(not(y+pixel_size+1 > height) and pixel_color(x,y+pixel_size) != (0,0,0) and pixel_color(x,y+pixel_size) != (128,128,128)):
-		info.append([x,y+pixel_size])
-	if(not(x-pixel_size < 0) and pixel_color(x-pixel_size,y) != (0,0,0) and pixel_color(x-pixel_size,y) != (128,128,128)):
-		info.append([x-pixel_size,y])
+def poinf_info(x, y):
+	info = []
+	if(not(y-pixel_size < 0) and pixel_color(x, y-pixel_size) != (0, 0, 0) and pixel_color(x, y-pixel_size) != (128, 128, 128)):
+		info.append([x, y-pixel_size])
+	if(not(x+pixel_size+1 > width) and pixel_color(x+pixel_size, y) != (0, 0, 0) and pixel_color(x+pixel_size, y) != (128, 128, 128)):
+		info.append([x+pixel_size, y])
+	if(not(y+pixel_size+1 > height) and pixel_color(x, y+pixel_size) != (0, 0, 0) and pixel_color(x, y+pixel_size) != (128, 128, 128)):
+		info.append([x, y+pixel_size])
+	if(not(x-pixel_size < 0) and pixel_color(x-pixel_size, y) != (0, 0, 0) and pixel_color(x-pixel_size, y) != (128, 128, 128)):
+		info.append([x-pixel_size, y])
 	return info
 
 
-
 def execute():
-	arr,start,end = make()
+	global on
+	arr, start, end = make()
 	que = [start]
 
-
-	x_start,y_start = start
-	S = [x_start//pixel_size,y_start//pixel_size]
+	x_start, y_start = start
+	S = [x_start//pixel_size, y_start//pixel_size]
 
 	x_end, y_end = end
-	E = [x_end,y_end]
+	E = [x_end, y_end]
 	i = 0
 	while(arr[x_end//pixel_size][y_end//pixel_size] == 0):
 		if(len(que) == 0):
@@ -101,59 +100,58 @@ def execute():
 			
 			if(arr_of_visiting[element] not in que):
 				que.append(arr_of_visiting[element])
-				x,y = arr_of_visiting[element]
-				arr[x//pixel_size][y//pixel_size] = [que[0][0]//pixel_size,que[0][1]//pixel_size]
+				x, y = arr_of_visiting[element]
+				arr[x//pixel_size][y//pixel_size] = [que[0][0]//pixel_size, que[0][1]//pixel_size]
 
-				if(S != [x//pixel_size,y//pixel_size] and E != [x,y]):
-					platno.create_rectangle(x+1,y+1,x+pixel_size-1,y+pixel_size-1, fill = "grey")
-					draw.rectangle([x+1,y+1,x+pixel_size-1,y+pixel_size-1], fill = "grey", outline = "grey")
+				if(S != [x//pixel_size, y//pixel_size] and E != [x, y]):
+					screen.create_rectangle(x+1, y+1, x+pixel_size-1, y+pixel_size-1, fill = "grey")
+					draw.rectangle([x+1, y+1, x+pixel_size-1, y+pixel_size-1], fill = "grey", outline = "grey")
 					
-					platno.update()
+					screen.update()
 					time.sleep(0.001)
 		que.pop(0)
 
-	
 	E = arr[x_end//pixel_size][y_end//pixel_size]
 	step = 0
 	walker = E
 
 	while(walker != S):
+		on = 1
 		x = walker[0]
 		y = walker[1]
-		platno.create_rectangle(x*pixel_size+1,y*pixel_size+1,x*pixel_size+pixel_size-1,y*pixel_size+pixel_size-1, fill = "light goldenrod", outline = "light goldenrod")
+		screen.create_rectangle(x*pixel_size+1, y*pixel_size+1, x*pixel_size+pixel_size-1, y*pixel_size+pixel_size-1, fill = "light goldenrod", outline = "light goldenrod")
 		walker = arr[walker[0]][walker[1]]
 		
-		platno.update()
+		screen.update()
 		time.sleep(0.1)
 		step += 1
+	on = 0
 
+
+
+width, height, pixel_size = set_size()
 
 root = Tk()
 root.resizable(False, False)
-root.title("P A T H    F I N D E R")
 root.geometry(f'{width}x{height+52}')
+root.title("P A T H    F I N D E R")
 
-platno = Canvas(root, width = width, height = height, bg = "white")
-platno.pack(side="top")
+screen = Canvas(root, width = width, height = height, bg = "white")
+screen.pack(side="top")
 
-image = PIL.Image.new("RGB", (width,height), "white")
+image = PIL.Image.new("RGB", (width, height), "white")
 draw = ImageDraw.Draw(image)
 
-def create_grid(width,height,pixel_size):	#making the grid
-	for riadok in range(pixel_size,height,pixel_size):
-		platno.create_line(0,riadok,width,riadok, fill = "grey")
-	for stlpec in range(pixel_size,width,pixel_size):
-		platno.create_line(stlpec,0,stlpec,height, fill = "grey")
+create_grid(width, height, pixel_size)
 
-create_grid(width,height,pixel_size)
+screen.bind("<Button-1>", paint)
+screen.bind("<B1-Motion>", paint)
 
-platno.bind("<Button-1>", paint)
-platno.bind("<B1-Motion>", paint)
-
-but_exe = Button(root, width = 40, height = 2, text = "EXECUTE", command = execute).place(x = 4, y = height+2)
-but_str = Button(root, width = 40, height = 2, text = "START BLOCK", command = set_red).place(x = 365, y = height+2)
-but_fin = Button(root, width = 40, height = 2, text = "FINISH BLOCK", command = set_blue).place(x = 726, y = height+2)
-but_obs = Button(root, width = 40, height = 2, text = "OBSTACLE BLOCK", command = set_black).place(x = 1087, y = height+2)
-but_era = Button(root, width = 40, height = 2, text = "ERASE", command = set_white).place(x = 1448, y = height+2)
+but_exe = Button(root, width = 33, height = 2, text = "EXECUTE", command = execute).place(x = 2, y = height+2)
+but_str = Button(root, width = 33, height = 2, text = "START BLOCK", command = lambda: set_color('red')).place(x = 303, y = height+2)
+but_fin = Button(root, width = 33, height = 2, text = "FINISH BLOCK", command = lambda: set_color('blue')).place(x = 604, y = height+2)
+but_obs = Button(root, width = 33, height = 2, text = "OBSTACLE BLOCK", command = lambda: set_color('black')).place(x = 905, y = height+2)
+but_era = Button(root, width = 33, height = 2, text = "ERASE", command = lambda: set_color('white')).place(x = 1206, y = height+2)
+but_all = Button(root, width = 33, height = 2, text = "ERASE ALL", command = erase_all).place(x = 1506, y = height+2)
 
 root.mainloop()
